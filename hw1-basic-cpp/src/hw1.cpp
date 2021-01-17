897ba9c63542b612513b052ba97f711d03f8ce0a
sabrinaspage <sabreyes01@gmail.com>
2021-01-17 04:05:44 -0500

    hw1

---
+++ hw1-basic-cpp/src/hw1.cpp
@@ -0,0 +1,48 @@
+#include "hw1.h"
+
+// Put your functions/prototype here if you need to 
+
+
+
+
+
+// Question 1
+void fizzBuzz() {
+	//TODO
+}
+
+// Question 2
+void findLargeSmall() {
+	//TODO
+}
+
+// Question 3
+void fillArray(char ar[], size_t& size) {
+	//TODO
+}
+
+// Question 4
+char highestOccurrences(const char ar[], const size_t size) {
+	//TODO
+}
+
+void printLargeSmall(const int large, const int small) {
+	cout << "The largest is " << large << endl;
+	cout << "The smallest is " << small << endl;
+}
+
+// Question 5
+void shiftAr(char ar[], const size_t size, const int numShift) {
+	//TODO
+}
+
+// Question 6
+char* merge(const char ar1[], const size_t n1, const char ar2[], const size_t n2) {
+	//TODO
+}
+
+void printAr(const char ar[], const size_t size) {
+	for(size_t i = 0; i < size; i++)
+		cout << ar[i];
+	cout << endl;
+}
